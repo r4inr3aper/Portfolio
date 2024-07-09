e977905053799d4e41f0af86cade56bf87ebc5a8
Bedanta Kataki <132095134+r4inr3aper@users.noreply.github.com>
2024-07-10 03:18:28 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/r4inr3aper'
+      className='link footer__link'
+    >
+      Created By Bedanta Kataki
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,66 @@
+const header = {
+  homepage: '#',
+  title: 'bk.',
+}
+
+const about = {
+  name: 'Bedanta Kataki',
+  role: 'Full Stack Developer',
+  description:
+    'I\'m studying Electronics and Instrumentation Engineering at NIT Silchar, driven by a deep fascination for data structures and algorithms. Specializing in creating efficient and scalable web applications, I love tackling complex challenges and transforming ideas into reality. Let\'s build something extraordinary together!',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/bedanta-kataki-0b5205257/',
+    github: 'https://github.com/r4inr3aper',
+  },
+}
+
+const projects = [
+  {
+    name: 'Flick: A Full Stack Food Ordering App',
+    description:
+      'A Full-Stack Food Ordering App using MERN stack and SCSS, including an admin panel.',
+    stack: ['ReactJS', 'SCSS', 'ExpressJs', 'MongoDB'],
+    sourceCode: 'https://github.com/r4inr3aper/Flick',
+    livePreview: '',
+  },
+  {
+    name: 'Incandescence 2024 Website',
+    description:
+      'A Fest website with dedicated sections for Events, Sponsors, Gallery, using NextJS and SCSS.',
+      stack: ['NextJS', 'SCSS', 'ExpressJs', 'MongoDB'],
+      sourceCode: 'https://github.com/Ecell-NITS/Incand24-frontend',
+    livePreview: 'https://www.incand.co.in/',
+  },
+  {
+    name: 'E-Cell Official Website',
+    description:
+      'Developed an advanced admin panel for the website, enabling the addition of events and blogs, and displaying user data ensuring efficient content management and user data handling.',
+      stack: ['ReactJS', 'SCSS', 'ExpressJs', 'MongoDB'],
+      sourceCode: 'https://github.com/Ecell-NITS/e-cell-website-22',
+    livePreview: 'https://ecellnits.org/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'SCSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'NextJS',
+  'ExpressJs',
+  'MongoDB',
+  'SQL',
+  'Prisma',
+  'Material UI',
+  'Git',
+]
+
+const contact = {
+  email: 'bedantak_ug_22@ei.nits.ac.in}',
+}
+
+export { header, about, projects, skills, contact }
